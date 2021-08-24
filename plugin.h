a48767e1a517745b59864c98e39389a3899c0d41
ubelhj <ubelhj@uw.edu>
2021-08-23 20:11:24 -0700

    adds plugin settings starter code

--- plugin.h
+++ plugin.h
@@ -4,12 +4,13 @@
 #include "bakkesmod/plugin/pluginwindow.h"
 
 #include "NetcodeManager/NetcodeManager.h"
+#include "bakkesmod/plugin/PluginSettingsWindow.h"
 
 #include "version.h"
 constexpr auto plugin_version = stringify(VERSION_MAJOR) "." stringify(VERSION_MINOR) "." stringify(VERSION_PATCH) "." stringify(VERSION_BUILD);
 
 
-class $projectname$: public BakkesMod::Plugin::BakkesModPlugin/*, public BakkesMod::Plugin::PluginWindow*/
+class $projectname$: public BakkesMod::Plugin::BakkesModPlugin/*, public BakkesMod::Plugin::PluginSettingsWindow*//*, public BakkesMod::Plugin::PluginWindow*/
 {
 private:
     std::shared_ptr<NetcodeManager> Netcode;
@@ -29,6 +30,13 @@ public:
 	// https://github.com/CinderBlocc/NetcodeManagerExample
 	// https://youtu.be/jyFuN8k2uIs
 	// void RequestBall();
+
+	// Inherited via PluginSettingsWindow
+	/*
+	void RenderSettings() override;
+	std::string GetPluginName() override;
+	void SetImGuiContext(uintptr_t ctx) override;
+	*/
 
 	// Inherited via PluginWindow
 	/*
--- pluginGUI.cpp
+++ pluginGUI.cpp
@@ -1,6 +1,24 @@
 #include "pch.h"
 #include "$projectname$.h"
 
+// Plugin Settings Window code here
+/*
+std::string $projectname$::GetPluginName() {
+	return "$projectname$";
+}
+
+void $projectname$::SetImGuiContext(uintptr_t ctx) {
+	ImGui::SetCurrentContext(reinterpret_cast<ImGuiContext*>(ctx));
+}
+
+// Render the plugin settings here
+// This will show up in bakkesmod when the plugin is loaded at
+//  f2 -> plugins -> $projectname$
+void $projectname$::RenderSettings() {
+	ImGui::TextUnformatted("$projectname$ plugin settings");
+}
+*/
+
 /*
 // Do ImGui rendering here
 void $projectname$::Render()
